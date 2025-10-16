3a7761056f36b6b786698a68264e45792f2b79e5
SabaFadhl <sabafadl98@gmail.com>
2025-10-16 12:51:44 +0300

    feat: create appbar with some properties

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,7 +3,22 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BreakFast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+
+      ),
+    );
   }
   
 }
