9d2fa38ea9aa3d9f27da1a02b4dd274702db0c4e
SabaFadhl <sabafadl98@gmail.com>
2025-10-16 22:23:31 +0300

    feat: app bar icons

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: false,
       // fontFamily:
       ),
       home: HomePage(),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,15 +10,27 @@ class HomePage extends StatelessWidget{
           style: TextStyle(
             color: Colors.black,
             fontSize: 18,
-            fontWeight: FontWeight.bold
+            fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
         elevation: 0.0,
         backgroundColor: Colors.white,
-
+        leading: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          child: Icon(Icons.arrow_back_ios, color: Colors.grey, size: 30,),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        ),
+        actions: [
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(10),
+            child: Icon(Icons.more_horiz, color: Colors.grey, size: 30,),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+          ),
+        ],
       ),
     );
   }
-  
 }
