53335dc46039ffb1eb3f6cf04082bf1c598caf18
SabaFadhl <sabafadl98@gmail.com>
2025-10-16 22:50:33 +0300

    feat: Search textbox and icons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -36,6 +36,34 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+      body:Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40,left: 20,right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(color: Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0)
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.all(15),
+                  prefixIcon: Icon(Icons.search_rounded),
+                  suffixIcon: Icon(Icons.filter_list),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none
+                  )
+
+              ),
+            ),
+          )
+        ],
+      ) ,
     );
   }
 }
